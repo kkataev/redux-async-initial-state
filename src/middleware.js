a2579436b00c3eb211c79c27e63f31c07e7f077d
Tyler Untisz <tuntisz@gmail.com>
2017-09-21 22:19:00 -0600

    Passes get state as a function in second parameter

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ export default load => store => {
   store.dispatch({
     type: STATE_LOADING_START,
   });
-  load(store.getState()).then(
+  load(store.getState, store.getState()).then(
     state => {
       store.dispatch({
         type: STATE_LOADING_DONE,
